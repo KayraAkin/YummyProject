2686e8e3972655ddf2d743c201c4f27e0d0605df
kayra akın <kayrakin11@hotmail.com>
2025-04-22 18:05:53 +0300

    update

--- YummyProject/Controllers/ChefController.cs
+++ YummyProject/Controllers/ChefController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using YummyProject.Context;
 using YummyProject.Models;
+
 
 namespace YummyProject.Controllers
 {
@@ -43,6 +45,18 @@ namespace YummyProject.Controllers
         {
             var value = _context.Chefs.Find(id);
             _context.Chefs.Remove(value);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UpdateChef(int id)
+        {
+            var value = _context.Chefs.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateChef(Chef chef)
+        {
+            _context.Entry(chef).State = EntityState.Modified;
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
